1da16ecb42cdba508b9b11259105238390ed707f
EricBoix <eric.boix@insa-lyon.fr>
2026-09-10 14:25:26 +0200

    Bug fix

--- jejune_cli/component_ecosystem.py
+++ jejune_cli/component_ecosystem.py
@@ -76,10 +76,11 @@ class comp_ecosystem(component):
             return False
         from .plugin_package_catalog import PLUGIN_PACKAGE_CATALOG
         return any(
-            self.repo_status(PLUGIN_PACKAGE_CATALOG.get(comp.name), root_dir, tmp_dir)[0] == "remote"
+            self.repo_status(pkg_name, root_dir, tmp_dir)[0] == "remote"
             for comp in ComponentRegistry()
             if comp in active
             if getattr(comp, "repos", [])
+            if (pkg_name := PLUGIN_PACKAGE_CATALOG.get(comp.name)) is not None
         )
 
     def check(self) -> tuple[str, str]:
